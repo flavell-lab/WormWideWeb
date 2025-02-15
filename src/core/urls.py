d446aa4fbd72e6446ca7101c43ba6917c57e7b58
urlicht <urlicht@users.noreply.github.com>
2025-02-15 22:37:51 +0900

    health check endpoint

--- src/core/urls.py
+++ src/core/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('', views.index, name='index'),
+    path('', views.index, name='index'),
     path('about/', views.about, name="about"),
+    path('is_healthy/', views.is_healthy, name="health_check")
 ]
--- src/core/views.py
+++ src/core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.decorators.cache import cache_page
+from django.http import HttpResponse
+from django.db import connection
 
 @cache_page(60*60*24*30)
 def index(request):
@@ -12,3 +14,10 @@ def about(request):
     context = {}
 
     return render(request, "core/about.html", context)
+
+def is_healthy(request):
+    try:
+        connection.ensure_connection()
+        return HttpResponse("OK", status=200)
+    except Exception as e:
+        return HttpResponse("ERROR", status=500) # failure
