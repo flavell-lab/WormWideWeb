9dea88413f0140b9de7970cdf83a610d7cb189b9
urlicht <urlicht@users.noreply.github.com>
2026-03-28 09:42:54 +0000

    remove build scripts (moved to gcp build config)

--- gcp_build.sh
+++
@@ -1,9 +0,0 @@
-# build
-docker compose build
-
-# remove the config file
-rm -f config/default.env
-
-# tag and push to artifact repo
-docker tag gcp_deploy:latest us-central1-docker.pkg.dev/www-deploy/www-docker/gcp_deploy:latest
-docker push us-central1-docker.pkg.dev/www-deploy/www-docker/gcp_deploy:latest
--- gcp_prebuild.sh
+++
@@ -1,5 +0,0 @@
-# copy data
-gsutil -m -q cp -r gs://www-build-bucket/initial_data .
-
-# create build config with build-only runtime values
-bash create_build_config.sh
